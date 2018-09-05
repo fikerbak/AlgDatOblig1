bb669b7c5508b62abe6e14d7ba5f4e8ae3519ea8
Esmaeel <esmaeeelu@yahoo.com>
2018-09-05 16:03:42 +0200

    Oppgave 5

--- src/oblig1.java
+++ src/oblig1.java
@@ -1,4 +1,23 @@
 public class oblig1 {
 
+    // Oppgave 5
+    public static void main(String[] args) {
+        char[] a = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J'};
+
+        char[] b = Main.rotasjon(a);
+        System.out.println(b);
+    }
+
+    public static char[] rotasjon(char[] a){
+
+        for ( int i = 0; i < a.length; i++){
+            char temp = a[i];
+            a[i]= a[a.length-1];
+            a[a.length-1] = temp;
+        }
+        return a ;
+    }
+}
+
 
 }
