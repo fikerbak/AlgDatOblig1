f770046bca04ced5364fd1daefbe9fb1217e96a1
hassansamatar <36266481+hassansamatar@users.noreply.github.com>
2018-09-09 10:27:37 +0200

    Update oblig1.java
    Oppgavene (2, 8, 9).

--- src/oblig1.java
+++ src/oblig1.java
@@ -69,6 +69,28 @@ public class oblig1 {
 
 }
 
+//oppgave 2
+    public static int antallUlikeSortert(int[]a){
+
+        if(a.length<2){
+            return a.length;
+        }
+        int antallUlike=1;
+        for(int i=1; i<a.length; i++){
+            if (a[i-1]>a[i]){
+                throw new IllegalStateException("Tabellen er usorter");
+            }else{
+                if(a[i-1]<a[i]){
+                    antallUlike++;
+                }
+            }
+
+        }
+        System.out.println(" Antall ulike sortert tall : "+antallUlike);
+        return  antallUlike;
+    }
+
+
 
 
 // oppgave 5
@@ -103,5 +125,69 @@ public static void rotasjon(char[] a, int k) {
         }
     }
 }
+
+
+
+//Oppgave 8
+    public static int[] indekssortering(int[]a){
+        int[] indeks = new int[a.length];
+        for(int i=0; i<a.length; i++){
+            indeks[i]=i;
+        }
+          int temp;
+           int  j;
+        for(int i=1; i<a.length; i++){
+            for (temp=indeks[i], j=i-1; j>=0 && a[temp]<a[indeks[j]]; j--){
+                indeks[j+1]=indeks[j];
+            }
+            indeks[j+1]=temp;
+        }
+        return indeks;
+    }
+
+
+
+    // Oppgave 9
+    public static int[] tredjeMin(int[]a){
+        int n=a.length; // lengde av tabbelen
+        if(n<3){ // Tabellen må ha minste 3 verdier
+            throw new NoSuchElementException("a.length("  +n+ ")<3!");
+        }
+
+       int[] indeks= indekssortering(Arrays.copyOf(a,3));
+        int m= indeks[0]; // m er indeksen til minste verdi
+        int nm= indeks[1]; // nm  er indeksen til  neste minste verdi
+        int tm= indeks[2]; // tm  er indeksen til tredje minste verdi
+
+        int minVerdi=a[m];    // minst verdi
+        int nesteMinVerdi=a[nm];
+        int tredjeMinVerdi=a[tm];
+        for(int i=3; i<n; i++){
+            int verdi=a[i];
+            if(verdi<tredjeMinVerdi){
+                if(verdi<nesteMinVerdi){
+                    if(verdi<minVerdi){
+                        tm=nm;
+                        tredjeMinVerdi=nesteMinVerdi;
+                        nm=m;
+                        nesteMinVerdi=minVerdi;
+                        m=i;
+                        minVerdi=verdi;
+                    }else {
+                        tm=nm;
+                        tredjeMinVerdi=nesteMinVerdi;
+                        nm=i;
+                        nesteMinVerdi=verdi;
+                    }
+                }else {
+                    tm=i;
+                    tredjeMinVerdi=verdi;
+                }
+            }
+        }
+
+      return new int[]{m, nm, tm} ;
+
+    }
 
 
